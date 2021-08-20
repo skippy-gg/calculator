1daedab9738bb57b733424c2009e8135f703cfeb
skippy-gg <45381691+skippy-gg@users.noreply.github.com>
2021-08-19 21:43:51 -0400

    Initial commit

--- script.js
+++ script.js
@@ -4,96 +4,125 @@ const MULTIPLY = "×";
 const ADD = "+";
 const SUBTRACT = "−";
 const EQUALS = "=";
-const CLEAR = "Clear";
+const CLEAR = "A/C";
 const DELETE = "Delete";
 
-//Assigning DOM Elements
-const workingValues = document.getElementById('workingValues');
-const finalValues = document.getElementById('finalValues');
+//Define operator array
+const calcOperators = [DIVIDE, MULTIPLY, ADD, SUBTRACT];
 
+//Define top and bottom text DOM elements
+const topExpr = document.getElementById("topValues");
+const botExpr = document.getElementById("bottomValues");
 
-const operators = [DIVIDE, MULTIPLY, ADD, SUBTRACT];
-const calcFunctions = [EQUALS, CLEAR, DELETE];
+//Global Variables
+let currValue = '0';
+let prevValue = '';
+let operator = '';
+let last = false;
 
-let currExpression = "";
-let currOperator = "";
-
-document.querySelectorAll('.calculatorBtn').forEach(button => {
-    button.addEventListener('click', testLOL)});
-
-function add(a,b){
-    return a+b;
-}
-
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
-}
-
+//Calculator math functions
 function operation(operator, a, b){
     if (operator == ADD){
-        return add(a,b);
+        return a+b;
     }
 
     else if (operator == SUBTRACT){
-        return subtract(a,b);
+        return a-b;
     }
 
     else if (operator == MULTIPLY){
-        return multiply(a,b);
+        return a*b;
     }
     
     else if (operator == DIVIDE){
-        return divide(a,b);
+        return a/b;
     }
 }
 
-function testLOL(e){
-    if (operators.includes(e.target.textContent)){
-        currOperator = e.target.textContent;
+//Updates current expression 
+function currExpression(e){
+    if (botExpr.textContent == "0" | last){
+        currValue = e.target.textContent;
+        last = false;
     }
-
-    if (e.target.textContent == CLEAR){
-        finalValues.textContent = "0";
-        currExpression = "";
-        workingValues.textContent = "";
-    }
-
-    if (e.target.textContent == DELETE & currExpression.length>=1){
-        currExpression = currExpression.slice(0,(currExpression.length)-1);
-        workingValues.textContent = currExpression;
-    }
-
-    if (!calcFunctions.includes(e.target.textContent)){
-        if (currOperator == DIVIDE & e.target.textContent == "0"){
-            alert("You cannot divide by 0!");
-            return;
-        }
-    currExpression+=(e.target.textContent);
-    workingValues.textContent = currExpression;
-    }
-
-    if (e.target.textContent == EQUALS){
-        console.log(currOperator);
-        let test = currExpression.split(currOperator);
-        console.log(currExpression);
-        console.log(test);
-        if (currExpression.includes(".")){
-            a = parseFloat(test[0]);
-            b = parseFloat(test[1]);
-        }
-        else{
-        a = parseInt(test[0]);
-        // operator = test[1];
-        b = parseInt(test[1]);
-        }
-        finalValues.textContent = operation(currOperator, a, b);
+    else{
+        currValue += e.target.textContent;
+        last = false;
     }
 }
+
+//Updates previous expression
+function prevExpression(){
+    prevValue = currValue+operator;
+}
+
+//Stores recently pressed operator
+function setOperator(e){
+    operator = e.target.textContent;
+    last = true;
+}
+
+//Evaluates the current expression and previous expression using the operator
+function evalExpression(){
+    if (operator == DIVIDE & currValue == "0"){
+        alert("You cannot divide by zero!");
+        return;
+    }
+    [a, b] = getOperands();
+    prevValue += currValue;
+    currValue = operation(operator, a, b);
+}
+
+//Convers current, previous values to integers/floats
+function getOperands(){
+    a = currValue.includes(".") ? parseFloat(currValue) : parseInt(currValue);
+    b = prevValue.includes(".") ? parseFloat(prevValue) : parseInt(prevValue);
+    return [a,b];
+}
+//Updates display text on calculator
+function updateDisplay(){
+    topExpr.textContent = prevValue;
+    botExpr.textContent = currValue;
+}
+
+//Clears prev and current values
+function clearAll(){
+    currValue = '0';
+    prevValue = '';
+}
+
+//Clears last digit in current value
+function clearCurrent(){
+    currValue.length<=1 ? currValue = '0' : currValue = currValue.slice(0,-1)
+}
+
+//EVENT LISTENERS
+document.getElementById("clearBtn").addEventListener('click',() => {
+    clearAll();
+    updateDisplay();
+})
+
+document.getElementById("deleteBtn").addEventListener('click',() => {
+    clearCurrent();
+    updateDisplay();
+})
+
+document.querySelectorAll(".operand").forEach(button => {
+    button.addEventListener('click',(e) => {
+    currExpression(e);
+    updateDisplay();
+    });
+});
+
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener('click',(e) => {
+    setOperator(e);
+    prevExpression();
+    updateDisplay();
+    });
+});
+
+document.querySelector(".equate").addEventListener('click',() => {
+    evalExpression();
+    updateDisplay();
+});
